11ccef916fb4d06880be6ad2559bf20608aa364a
Raviteja72 <krtproperty72@gmail.com>
2021-01-03 01:14:53 +0530

    CCL_DELETION

---
+++ LINKED_LISTS/CIRCULAR_LL/deletion_p.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main();
+struct node
+{
+    int data;
+    struct node *next;
+}*new, *head, *tail, *temp, *nift, *tsla;
+
+int main(void)
+{
+    int value, i, pos;
+    char ch;
+    // Creating LINKED LIST
+    do
+    {
+        new = (struct node *)malloc(sizeof(struct node));
+        printf("Enter value : ");
+        scanf("%d", &value);
+        new->data = value;
+        new->next = NULL;
+        if(head==NULL)
+        {
+            head = new;
+            tail = new;
+        }
+        else
+        {
+            tail->next = new;
+            tail = new;
+            tail->next = head;
+        }
+        printf("Y or N");
+        fflush(stdin);
+        scanf("%c", &ch);
+        }while(ch=='y');
+    // Printing LINKED LIST
+    temp = head;
+    printf("\nThe LINKED LIST : ");
+    while(temp->next!=head)
+    {
+        printf("\t%d", temp->data);
+        temp = temp->next;
+    }
+    printf("\t%d", temp->data);
+    // Deleting at SPECIFIC POINT
+    tsla = head;
+    printf("\nEnter position : ");
+    scanf("%d", &pos);
+    for(i=0;i<pos-1;i++)
+    {
+        tsla = tsla->next;
+        tsla->next = tsla->next->next;
+    }
+    nift = head;
+    printf("\n\nAfter Deletion at SPECIFIC POINT  \n\n NEW LINKED LIST : ");
+    while(nift->next!=head)
+    {
+        printf("\t%d", nift->data);
+        nift = nift->next;
+    }
+    printf("\t%d", nift->data);
+    return 0;
+}
